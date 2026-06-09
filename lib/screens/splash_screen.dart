baefd2aa3ece4b916f885403ac01ce551d9e23c0
afroeltechologies <afroeltechologies@gmail.com>
2026-06-09 03:13:42 +0000

    Fix duplicate import properly

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'books_home.dart';
-import 'books_home.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
